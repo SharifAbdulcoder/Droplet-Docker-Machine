da2161d1b815f1ad6db3849aa6e5be8520f38abb
SharifAbdulcoder <43179042+SharifAbdulcoder@users.noreply.github.com>
2018-10-02 19:48:14 -0500

    Update droplet.sh

--- droplet.sh
+++ droplet.sh
@@ -4,6 +4,6 @@ read -p "So tell me, what kind of virtual slave do you wish to install?: " slave
   echo "Thats great, I will need few information from you"
 read -p "Please enter your token: " token
 read -p "Enter digitalocean image: " image
-read -p "Please enter a drplet name: " name
+read -p "Please enter a droplet name: " name
    echo "Now creating $slave as you commanded!"
 docker-machine create --driver digitalocean --digitalocean-access-token $token --digitalocean-image "$image" $name
