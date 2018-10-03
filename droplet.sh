40164d2df7a2fa35c335dd96b69012300be5619a
SharifAbdulCoder <abdugofir@gmail.com>
2018-10-02 19:47:16 -0500

    Docker-Machine

---
+++ droplet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Hello there, I am your personal assistant 58449. I will help you create your vertual slave: "
+read -p "So tell me, what kind of virtual slave do you wish to install?: " slave
+  echo "Thats great, I will need few information from you"
+read -p "Please enter your token: " token
+read -p "Enter digitalocean image: " image
+read -p "Please enter a drplet name: " name
+   echo "Now creating $slave as you commanded!"
+docker-machine create --driver digitalocean --digitalocean-access-token $token --digitalocean-image "$image" $name
